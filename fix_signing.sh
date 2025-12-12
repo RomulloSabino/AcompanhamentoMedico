5ac9f255bfeaaa5534f8f8d123528de7d4e81c49
RomulloSabino <romullosabino@gmail.com>
2025-12-12 13:48:09 -0300

    atualizando

--- fix_signing.sh
+++ fix_signing.sh
@@ -5,14 +5,18 @@ PROJECT_FILE="platforms/ios/Saudi-AcomMed.xcodeproj/project.pbxproj"
 
 echo "Limpando configurações manuais de assinatura..."
 
-# Remove todas as linhas relacionadas à assinatura manual
-sed -i '' '/CODE_SIGN_STYLE/d' "$PROJECT_FILE"
+# REMOVE TODAS AS VARIAÇÕES DE CODE_SIGN_IDENTITY
 sed -i '' '/CODE_SIGN_IDENTITY/d' "$PROJECT_FILE"
-sed -i '' '/DEVELOPMENT_TEAM =/d' "$PROJECT_FILE"
-sed -i '' '/PROVISIONING_PROFILE /d' "$PROJECT_FILE"
+sed -i '' '/CODE_SIGN_IDENTITY\[/d' "$PROJECT_FILE"
+
+# REMOVE VARIÁVEIS DE PERFIS DE ASSINATURA
+sed -i '' '/PROVISIONING_PROFILE/d' "$PROJECT_FILE"
 sed -i '' '/PROVISIONING_PROFILE_SPECIFIER/d' "$PROJECT_FILE"
 
-# Força o Xcode a usar assinatura automática
+# REMOVE TEAM DEFINIDO EM BLOCOS ANTIGOS
+sed -i '' '/DEVELOPMENT_TEAM =/d' "$PROJECT_FILE"
+
+# FORÇA ASSINATURA AUTOMÁTICA
 echo "Inserindo DEVELOPMENT_TEAM e CODE_SIGN_STYLE Automatic..."
 
 sed -i '' 's/buildSettings = {/buildSettings = {\nDEVELOPMENT_TEAM = SSADB2592Z;\nCODE_SIGN_STYLE = Automatic;/' "$PROJECT_FILE"
